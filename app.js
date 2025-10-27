fda4427a49d849f93e6d80ba0579d2d7ec402363
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-10-28 02:30:58 +0300

    add: static files

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// set template engine
+app.set('view engine', 'pug');
+
+
+// when the browser issues a GET for /assets/style.css, fire up the /assets endpoint
+// and look for the specific static files after /assets in the folder public
+app.use('/css', express.static(`${__dirname}/css`));
+app.use('/assets', express.static(`${__dirname}/assets`));
+app.use('/js', express.static(`${__dirname}/js`));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
