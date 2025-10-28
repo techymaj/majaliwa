c51b815cf329bac02d95126065059ccc6354069a
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-10-28 03:15:42 +0300

    fix: pug identation bug in navigation.pug

--- app.js
+++ app.js
@@ -5,15 +5,16 @@ const port = 3000
 // set template engine
 app.set('view engine', 'pug');
 
-
-// when the browser issues a GET for /assets/style.css, fire up the /assets endpoint
-// and look for the specific static files after /assets in the folder public
 app.use('/css', express.static(`${__dirname}/css`));
 app.use('/assets', express.static(`${__dirname}/assets`));
 app.use('/js', express.static(`${__dirname}/js`));
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/reality', (req, res) => {
+  res.render('reality')
 })
 
 app.listen(port, () => {
